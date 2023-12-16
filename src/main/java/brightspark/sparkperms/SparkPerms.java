7c1ad471ce23495b887460000c4385b0cccfb39a
bright_spark <markpassey@btinternet.com>
2023-12-16 04:00:41 +0000

    Added more logging

--- src/main/java/brightspark/sparkperms/SparkPerms.java
+++ src/main/java/brightspark/sparkperms/SparkPerms.java
@@ -15,6 +15,7 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggingEventBuilder;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -31,12 +32,14 @@ import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class SparkPerms implements ModInitializer {
+	private static final String MOD_ID = "sparkperms";
 	private static final Logger LOG = LoggerFactory.getLogger(SparkPerms.class);
-	private static final String PERMS_FILE_NAME = "sparkperms.txt";
+	private static final String PERMS_FILE_NAME = MOD_ID + ".txt";
 	private static final String REGEX_PERMISSION = "^(?:\\w+\\.?)+$";
-	private static final String COMMAND_PERM = "command.sparkperms";
+	private static final String COMMAND_PERM = "command." + MOD_ID;
 	private static final DynamicCommandExceptionType EXCEPTION_INVALID_PERM =
 		new DynamicCommandExceptionType(o -> Text.of("'" + o + "' is not a valid permission name"));
+
 	private final HashSet<String> allowedPerms = new HashSet<>();
 	private Path permsPath;
 
@@ -51,27 +54,35 @@ public class SparkPerms implements ModInitializer {
 		);
 	}
 
+	private static LoggingEventBuilder log(String message) {
+		return LOG.atInfo().setMessage(MOD_ID + ": " + message);
+	}
+
+	private static LoggingEventBuilder error(String message, Exception cause) {
+		return LOG.atError().setMessage(MOD_ID + ": " + message).setCause(cause);
+	}
+
 	private void loadPerms() {
-		LOG.atInfo().setMessage("Loading perms from {}").addArgument(permsPath).log();
+		log("Loading perms from {}").addArgument(permsPath).log();
 		allowedPerms.clear();
 		if (Files.exists(permsPath)) {
 			try (BufferedReader reader = Files.newBufferedReader(permsPath)) {
 				reader.lines().forEach(allowedPerms::add);
 			} catch (IOException e) {
-				LOG.atError().setMessage("Error loading perms from {}").setCause(e).addArgument(permsPath).log();
+				error("Error loading perms from {}", e).addArgument(permsPath).log();
 			}
 		} else {
 			try {
 				Files.createFile(permsPath);
 			} catch (IOException e) {
-				LOG.atError().setMessage("Error creating new perms file {}").setCause(e).addArgument(permsPath).log();
+				error("Error creating new perms file {}", e).addArgument(permsPath).log();
 			}
 		}
-		LOG.atInfo().setMessage("Loaded {} perms").addArgument(allowedPerms::size).log();
+		log("Loaded {} perms").addArgument(allowedPerms::size).log();
 	}
 
 	private void writePerms() {
-		LOG.atInfo().setMessage("Writing perms to {}").addArgument(permsPath).log();
+		log("Writing perms to {}").addArgument(permsPath).log();
 		try (BufferedWriter writer = Files.newBufferedWriter(permsPath, WRITE, CREATE, TRUNCATE_EXISTING)) {
 			List<String> allowedPermsSorted = allowedPerms.stream().sorted().toList();
 			for (String perm : allowedPermsSorted) {
@@ -79,16 +90,18 @@ public class SparkPerms implements ModInitializer {
 				writer.newLine();
 			}
 		} catch (IOException e) {
-			LOG.atError().setMessage("Error writing perms to {}").setCause(e).addArgument(permsPath).log();
+			error("Error writing perms to {}", e).addArgument(permsPath).log();
 		}
-		LOG.atInfo().setMessage("Wrote {} perms").addArgument(allowedPerms::size).log();
+		log("Wrote {} perms").addArgument(allowedPerms::size).log();
 	}
 
 	private TriState checkPermission(String permission) {
+		log("Checking permission {}").addArgument(permission).log();
 		if (allowedPerms.stream().anyMatch(permission::startsWith)) {
-			LOG.atInfo().setMessage("Allowing command with permission {}").addArgument(permission).log();
+			log("Allowing command with permission {}").addArgument(permission).log();
 			return TriState.TRUE;
 		}
+		log("Check failed for permission {}").addArgument(permission).log();
 		return TriState.DEFAULT;
 	}
 
